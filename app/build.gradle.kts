fef5cdafa5e2d17369dd93afd9c5e715d22232c7
Alvarogogal <agorsua0610@g.educaand.es>
2024-11-27 16:36:57 +0100

    .

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,6 +59,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    //implementation(libs.androidx.ui.lifetime)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.constraintlayout.compose)
     testImplementation(libs.junit)
--- app/src/main/java/com/example/loginpsn/MainActivity.kt
+++ app/src/main/java/com/example/loginpsn/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.loginpsn
 
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -18,6 +17,8 @@ import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.Button
+import androidx.compose.material3.TextFieldColors
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -35,6 +36,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.sp
 import com.example.loginpsn.ui.theme.LoginPSNTheme
 
@@ -49,11 +51,56 @@ class MainActivity : ComponentActivity() {
                     var textPasswd by rememberSaveable { mutableStateOf("")}
 
                     val id = "usuario@gmail.com"
-                    val contraseña = "123456789"
+                    val password = "1234"
+                    var colors = OutlinedTextFieldDefaults.colors(
+                        unfocusedTextColor = Color.LightGray,
+                        focusedTextColor = Color.White,
+                        unfocusedContainerColor = Color.Transparent,
+                        focusedContainerColor = Color.Transparent,
+                        focusedBorderColor = Color.White,
+                        unfocusedBorderColor = Color.LightGray,
+                        focusedPlaceholderColor = Color.LightGray,
+                        unfocusedPlaceholderColor = Color.LightGray
+                    )
 
-                    Login(Modifier.padding(innerPadding),textEmail,textPasswd,{textPasswd = it},{textEmail = it},id,contraseña)
+                    val colorsDef = OutlinedTextFieldDefaults.colors(
+                        unfocusedTextColor = Color.LightGray,
+                        focusedTextColor = Color.White,
+                        unfocusedContainerColor = Color.Transparent,
+                        focusedContainerColor = Color.Transparent,
+                        focusedBorderColor = Color.White,
+                        unfocusedBorderColor = Color.LightGray,
+                        focusedPlaceholderColor = Color.LightGray,
+                        unfocusedPlaceholderColor = Color.LightGray
+                    )
+                    val colorsBad = OutlinedTextFieldDefaults.colors(
+                        unfocusedTextColor = Color.LightGray,
+                        focusedTextColor = Color.White,
+                        unfocusedContainerColor = Color.Transparent,
+                        focusedContainerColor = Color.Transparent,
+                        focusedBorderColor = Color.Red,
+                        unfocusedBorderColor = Color.Red,
+                        focusedLabelColor = Color.Red,
+                        unfocusedLabelColor = Color.Red,
+                        focusedPlaceholderColor = Color.LightGray,
+                        unfocusedPlaceholderColor = Color.LightGray
+                    )
 
 
+                    Login(
+                        Modifier.padding(innerPadding)
+                        ,textEmail
+                        ,textPasswd
+                        ,colors
+                        ,{textPasswd = it}
+                        ,{textEmail = it}
+                        ,{
+                            colors = if (textEmail==id && textPasswd==password){
+                                colorsDef
+                            }else{
+                                colorsBad
+                            }
+                        })
                 }
             }
         }
@@ -62,13 +109,14 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Login(
-    modifier: Modifier,
-    textEmail:String,
-    textPasswd:String,
-    changePasswd:(String) ->Unit
-    ,changeEmail:(String) ->Unit
-    ,id:String,
-    contraseña:String){
+    modifier: Modifier
+    , textEmail:String
+    , textPasswd:String
+    ,colors: TextFieldColors
+    , changePasswd:(String) ->Unit
+    , changeEmail:(String) ->Unit
+    , onClick: () -> Unit
+    ){
 
     Column(
         modifier = modifier
@@ -95,72 +143,84 @@ fun Login(
         ) {
             val (logo, text1, text2, field1, field2, button) = createRefs()
             val bottomBarrier = createBottomBarrier(logo, text1)
+
             Image(
-                modifier = Modifier
-                    .size(65.dp, 55.dp)
-                    .padding(5.dp)
-                    .constrainAs(logo) {
-                        top.linkTo(parent.top)
-                        start.linkTo(parent.start)
-                        end.linkTo(text1.start)
-                    },
+                modifier = Modifier.size(65.dp, 55.dp)
+                .padding(5.dp)
+                .constrainAs(logo)
+                {
+                    top.linkTo(parent.top)
+                    start.linkTo(parent.start)
+                    end.linkTo(text1.start)
+                },
                 painter = painterResource(R.drawable.logo),
                 contentScale = ContentScale.FillBounds,
                 contentDescription = "Logo"
             )
 
-            Text(modifier = Modifier.constrainAs(text1) {
-                top.linkTo(parent.top)
-                start.linkTo(logo.end)
-                end.linkTo(parent.end)
-            }.padding(5.dp),
+            Text(
+                modifier = Modifier.constrainAs(text1)
+                {
+                    top.linkTo(parent.top)
+                    start.linkTo(logo.end)
+                    end.linkTo(parent.end)
+                }
+                .padding(5.dp),
                 text = "PlayStation Network",
                 color = Color.White,
                 fontSize = 30.sp,
                 fontWeight = FontWeight.Bold)
 
             Text(modifier = Modifier
-                .constrainAs(text2) {
+                .constrainAs(text2)
+                {
                     top.linkTo(bottomBarrier, margin = 5.dp)
                     start.linkTo(parent.start)
                     end.linkTo(parent.end)
-
-                }.padding(5.dp,30.dp,5.dp,15.dp),
+                }
+                .padding(5.dp,30.dp,5.dp,15.dp),
                 text = "Go online by signing in to PlayStation Network",
                 color = Color.White,
                 fontSize = 17.sp)
 
             CampoDeTexto(
                 textEmail,
+                colors,
                 changeEmail,
-                Modifier.constrainAs(field1){
+                Modifier.constrainAs(field1)
+                {
                     top.linkTo(text2.bottom)
                     start.linkTo(parent.start)
                     end.linkTo(parent.end)
                 },
                 stringResource(R.string.email),
-                stringResource(R.string.sign_in_id_email_address)
+                stringResource(R.string.sign_in_id_email_address),
+                VisualTransformation.None
             )
 
             CampoDeTexto(
                 textPasswd,
+                colors,
                 changePasswd,
-                Modifier.constrainAs(field2){
+                Modifier.constrainAs(field2)
+                {
                     top.linkTo(field1.bottom)
                     start.linkTo(parent.start)
                     end.linkTo(parent.end)
                 },
                 stringResource(R.string.password),
-                stringResource(R.string.password)
+                stringResource(R.string.password),
+                PasswordVisualTransformation()
             )
 
-            Boton(Modifier.constrainAs(button){
-                bottom.linkTo(parent.bottom)
+            Boton(
+                Modifier.constrainAs(button)
+                {
+                top.linkTo(field2.bottom)
                 start.linkTo(parent.start)
                 end.linkTo(parent.end)
-            },
-            textEmail,
-            textPasswd)
+                }
+                ,onClick)
         }
     }
 }
@@ -177,7 +237,14 @@ fun TitleText(modifier: Modifier, contentAlignment: Alignment,text: String) {
 
 
 @Composable
-private fun CampoDeTexto(value: String, x:(String) ->Unit , modifier:Modifier, placeHolder: String, label: String){
+private fun CampoDeTexto(
+    value: String
+    ,colors: TextFieldColors
+    ,x:(String) ->Unit
+    ,modifier:Modifier
+    ,placeHolder: String
+    ,label: String
+    ,transformation: VisualTransformation){
         Column(modifier=modifier) {
             Spacer(Modifier.size(5.dp))
             Text(text = label,color=Color.White)
@@ -187,22 +254,15 @@ private fun CampoDeTexto(value: String, x:(String) ->Unit , modifier:Modifier, p
                 enabled = true,
                 onValueChange = x,
                 shape = RectangleShape,
-                visualTransformation = PasswordVisualTransformation(),
-                colors = OutlinedTextFieldDefaults.colors(
-                    unfocusedTextColor = Color.LightGray,
-                    focusedTextColor = Color.White,
-                    unfocusedContainerColor = Color.Transparent,
-                    focusedContainerColor = Color.Transparent,
-                    focusedBorderColor = Color.White,
-                    unfocusedBorderColor = Color.LightGray,
-                    focusedPlaceholderColor = Color.LightGray,
-                    unfocusedPlaceholderColor = Color.LightGray
-                )
+                visualTransformation = transformation,
+                colors = colors
             )
         }
     }
 
 @Composable
-fun Boton(modifier: Modifier, textEmail: String, textPasswd: String) {
-    Button(modifier = modifier) { }
+fun Boton(modifier: Modifier,onClick: () -> Unit) {
+    Button(
+        modifier = modifier.padding(5.dp)
+        ,onClick = onClick) { }
 }
